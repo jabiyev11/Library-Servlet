494727c1383b7cdf503f1093c78cd6630a008f7f
jabiyev11 <jjabiyev19615@ada.edu.az>
2024-11-14 00:15:25 +0400

    Tomcat configured

---
+++ src/main/java/org/example/javidjabiyev/libraryservlet/servlet/HelloServlet.java
@@ -0,0 +1,26 @@
+package org.example.javidjabiyev.libraryservlet.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.write("<h1>Hello World</h1>");
+        writer.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
